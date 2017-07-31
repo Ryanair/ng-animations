b198a327e0c150c26da57c1b251c12d4ba32a812
Jakub Barczyk <barczykj@ryanair.com>
2017-07-31 11:49:44 +0200

    Finalise ng-animations/ng-animations-stage-3

--- ng-animations-stage-3/src/hero-list/hero-list.component.ts
+++ ng-animations-stage-3/src/hero-list/hero-list.component.ts
@@ -12,10 +12,13 @@ import { animate, query, transition, trigger, style, stagger, keyframes } from '
         query(':enter', style({ opacity: 0 }), { optional: true }),
         query(':enter', stagger('200ms', [
           animate('500ms ease-in', keyframes([
-            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            style({ opacity: 0, transform: 'translateY(-20px)' }),
             style({ opacity: 0.5, transform: 'translateY(5px)' }),
             style({ opacity: 1, transform: 'translateY(0)' })
           ])),
+        ]), { optional: true }),
+        query(':leave', stagger('100ms', [
+          animate('200ms ease-out', style({ opacity: 0 }))
         ]), { optional: true })
       ])
     ])
